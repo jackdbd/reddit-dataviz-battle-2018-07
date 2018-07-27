4481ef4b7f5998949341713c48db28f495e7ff37
jackdbd <jackdebidda@gmail.com>
2018-07-27 18:13:16 +0200

    Add some documentation

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,3 +1,22 @@
+/**
+ * Step module.
+ * The Step component triggers the updates of the Graphic component.
+ * Scrollama uses the Intersection Observer API. In such API, the DOM element
+ * observed is called "target". Basically this Step component is the target.
+ * Please note that Step1 and StepN could be different components, as long as
+ * they are wrapped in a ScrollText component.
+ *
+ * ScrollContainer
+ *   ScrollText
+ *     Step1
+ *     ...
+ *     StepN
+ *   ScrollGraphic
+ *     Graphic
+ * @module /components/Step
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API|MDN}
+ * @see {@link https://github.com/russellgoldenberg/scrollama|Scrollama}
+ */
 import React from "react";
 import styled from "styled-components";
 
